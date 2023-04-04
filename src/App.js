f2fb4cfcd3e3547464fb750fb970626ac2db49c7
Silas Green <scgreen160@gmail.com>
2023-04-04 17:31:17 -0600

    today today

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import "./App.css";
+import GamerData from "./components/GamerData/GamerData";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <div className="container"></div>
       <Home exact path="/" element={Home} />
+      <GamerData />
     </div>
   );
 };
